106e3fbb87c6c636619e85ee6019358e8e3836bd
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-08-23 19:15:54 +0200

    Add tss

--- platform_pc/x86-32/multiboot/crt0.cc
+++ platform_pc/x86-32/multiboot/crt0.cc
@@ -7,6 +7,8 @@
 
 extern "C" {
 
+#include "tss.h"
+
 extern int __stack_end;
 
 void
@@ -34,15 +36,22 @@ typedef struct
 } __attribute__((packed)) gdt_table_t;
 
 static_assert(sizeof(gdt_table_t) == 8, "Verifying size failed!");
-#define GDT_BASE_LOW(x) (x &0xFFFF)
-#define GDT_BASE_MID(x) ((x>>16) &0xFF)
-#define GDT_BASE_HIGH(x) ((x>>(16+8)) &0xFF)
+
+
+#define GDT_BASE_LOW(x) (x & 0xFFFF)
+#define GDT_BASE_MID(x) ((x>>16) & 0xFF)
+#define GDT_BASE_HIGH(x) ((x>>(16+8)) & 0xFF)
+
+__attribute__((section(".data")))
+TSS_io tss_io;
 
 #define GDT_ACCES_CXR_0 (1<<7 | 0 << 5 | 1<<4 | 1 <<3 | 0 << 2 | 1 << 1 | 0 << 0)
 #define GDT_ACCES_DRW_0 (1<<7 | 0 << 5 | 1<<4 | 0 <<3 | 0 << 2 | 1 << 1 | 0 << 0)
 
 #define GDT_LIMIT_LOW_4K(x) ((x>>12) & 0xFFFF)
 #define GDT_LIMIT_HIGH_4K(x) ((x>>(12+16)) & 0xF)
+#define GDT_LIMIT_LOW_1B(x) ((x) & 0xFFFF)
+#define GDT_LIMIT_HIGH_1B(x) (((x)>>(16)) & 0xF)
 #define GDT_GRANULARITY_4K_32 (1<<7 | 1 << 6)
 
 static
@@ -67,6 +76,15 @@ gdt_table_t gdt_table[] =
 			GDT_GRANULARITY_4K_32,
 			GDT_BASE_HIGH(0x00000000)
 		}, //data descriptor
+		{
+			GDT_LIMIT_LOW_1B(sizeof(TSS)-1),
+			0,
+			0,
+			(0<<7)|(0<<5)|(1<<3)|(1<<0), //TSS
+			GDT_LIMIT_HIGH_1B(sizeof(TSS)-1)|
+			0x40,
+			0,
+		}, //v86 TSS descriptor
 };
 
 struct  __attribute__((packed)) Gdt_pointer
@@ -98,6 +116,23 @@ __attribute__((section(".init"))) _start(void) {
 	asm volatile("jmp $0x08,%0" :: "i" (_pstart));
 }
 
+__attribute__((cdecl))
+__attribute__((section(".init")))
+static void tss_init(void)
+{
+	tss_io.t.io_base = (DWORD)(&(tss_io.io_map)) - (DWORD)(&(tss_io));
+    for (int i = 0; i < 2047; i++) tss_io.io_map[i] = 0;
+    tss_io.io_map[2047] = 0xFF000000;
+
+
+    gdt_table[3].base_low = GDT_BASE_LOW((unsigned long)&tss_io.t);
+    gdt_table[3].base_middle = GDT_BASE_MID((unsigned long)&tss_io.t);
+    gdt_table[3].base_high = GDT_BASE_HIGH((unsigned long)&tss_io.t);
+    gdt_table[3].access |= 1<<7; //now present
+    asm volatile("lgdt (%0) " :  : "r"(&gdt_pointer));
+	asm("ltr %w0" : : "r"(0x18));
+}
+
 void
 __attribute__((naked))
 __attribute__((section(".init"))) _pstart(void) {
@@ -112,5 +147,6 @@ __attribute__((section(".init"))) _pstart(void) {
 	asm volatile("movl %0, %%esp" : : "i"(&__stack_end-1));
 	asm volatile("movl %0, %%ebp" : : "i"(&__stack_end-1));
 	asm volatile("push $0");
+	asm volatile("call %P0" :: "i"(tss_init));
 	asm volatile("jmp $0x08,%0" :: "i"(_cstart));
 }
---
+++ platform_pc/x86-32/multiboot/tss.h
@@ -0,0 +1,65 @@
+/*
+ * tss.h
+ *
+ *  Created on: Aug 18, 2021
+ *      Author: Korneliusz Osmenda
+ */
+
+#ifndef PLATFORM_PC_X86_32_TSS_H_
+#define PLATFORM_PC_X86_32_TSS_H_
+
+typedef unsigned short WORD;
+typedef unsigned int DWORD;
+typedef unsigned char BYTE;
+
+typedef struct tss {
+    WORD back_link __attribute__ ((packed));
+    WORD _fill0 __attribute__ ((packed));
+    DWORD esp0 __attribute__ ((packed));
+    WORD ss0 __attribute__ ((packed));
+    WORD _fill1 __attribute__ ((packed));
+    DWORD esp1 __attribute__ ((packed));
+    WORD ss1 __attribute__ ((packed));
+    WORD _fill2 __attribute__ ((packed));
+    DWORD esp2 __attribute__ ((packed));
+    WORD ss2 __attribute__ ((packed));
+    WORD _fill3 __attribute__ ((packed));
+    DWORD cr3 __attribute__ ((packed));
+    DWORD eip __attribute__ ((packed));
+    DWORD eflags __attribute__ ((packed));
+    DWORD eax __attribute__ ((packed));
+    DWORD ecx __attribute__ ((packed));
+    DWORD edx __attribute__ ((packed));
+    DWORD ebx __attribute__ ((packed));
+    DWORD esp __attribute__ ((packed));
+    DWORD ebp __attribute__ ((packed));
+    DWORD esi __attribute__ ((packed));
+    DWORD edi __attribute__ ((packed));
+    WORD es __attribute__ ((packed));
+    WORD _fill5 __attribute__ ((packed));
+    WORD cs __attribute__ ((packed));
+    WORD _fill6 __attribute__ ((packed));
+    WORD ss __attribute__ ((packed));
+    WORD _fill7 __attribute__ ((packed));
+    WORD ds __attribute__ ((packed));
+    WORD _fill8 __attribute__ ((packed));
+    WORD fs __attribute__ ((packed));
+    WORD _fill9 __attribute__ ((packed));
+    WORD gs __attribute__ ((packed));
+    WORD _fill10 __attribute__ ((packed));
+    WORD ldt __attribute__ ((packed));
+    WORD _fill11 __attribute__ ((packed));
+    WORD trap __attribute__ ((packed));
+    WORD io_base __attribute__ ((packed));
+} TSS;
+
+static_assert(sizeof(TSS) == 0x68, "Verifying size failed!");
+
+typedef struct {
+    TSS t;
+    DWORD io_map[2048];
+} TSS_io;
+
+extern TSS_io tss_io;
+
+#endif /* PLATFORM_PC_X86_32_TSS_H_ */
