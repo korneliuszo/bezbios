180f3b1387b70ce6dcb931f93187c898451b8cb7
Korneliusz Osmenda <korneliuszo@gmail.com>
2021-09-27 14:07:02 +0200

    cxa_guard

---
+++ platform_pc/x86-32/cxa_guard.c
@@ -0,0 +1,25 @@
+/*
+ * cxa_guard.c
+ *
+ *  Created on: 27 wrz 2021
+ *      Author: nyuu
+ */
+
+#include "io.h"
+
+
+int __cxa_guard_acquire(unsigned long long * guard_object)
+{
+	unsigned long flags = cli();
+	if((unsigned char)*guard_object)
+	{
+		sti(flags);
+		return 0;
+	}
+	*guard_object = (*guard_object&0xffffffff) | (unsigned long long)flags << 32 | 1;
+	return 1;
+}
+
+void __cxa_guard_release(unsigned long long * guard_object) {
+	sti(*guard_object>>32);
+}
